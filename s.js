956a01445fbffdb5098facb4afc818a0c624484b
Eugen-13 <78321087+Eugen-13@users.noreply.github.com>
2021-02-02 21:29:19 +0300

    Add files via upload

---
+++ s.js
@@ -0,0 +1,10 @@
+window.addEventListener('load', function() {
+        let but1 = document.getElementById('b1');
+        let but2 = document.getElementById('b2');
+        but1.addEventListener("click",function() {
+            window.location.href = "https://www.vk.com/";
+        });
+    but2.addEventListener("click",function() {
+    document.body.style.background="gray";
+});
+});
